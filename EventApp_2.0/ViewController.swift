ae9cff04986fa15184ccdc6e2230fc0ee3b17017
Ivan Komarov <ikomarov@kse.org.ua>
2023-05-20 16:15:51 +0300

    Initial Commit

---
+++ EventApp_2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventApp_2.0
+//
+//  Created by Ivan Komarov on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
